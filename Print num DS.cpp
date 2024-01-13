be34c08822d3d1ba8e3eef7f3adcccdd0abd65e7
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2024-01-13 18:02:34 +0600

    Create Print num DS.cpp

---
+++ Print num DS.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num[5];
+    int i;
+    cout<<"Enter 5 number :";
+    for(int i=0;i<5;i++)
+    {
+        cin>>num[i];
+
+    }
+    for(i=0;i<5;i++)
+    {
+        cout<<"Number is: "<<num[i] <<endl;
+    }
+
+  return 0;
+}
