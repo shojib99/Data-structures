6765f27e25f1eeacc63d47e478b3b934e7a309fe
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2024-01-13 18:12:19 +0600

    Create sum DS.cpp

---
+++ sum DS.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num[20];
+    int num1, num2;
+    cout<<" Enter num1 :"<<endl;
+    cin>>num1;
+    cout<<" Enter num2 :"<<endl;
+    cin>>num2;
+    int sum;
+    sum=num1+num2;
+    cout<<" The sum is "<<sum<<endl;
+
+    return 0;
+}
