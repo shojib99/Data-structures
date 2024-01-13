9641ea039a125be96dbccd13d8586372ef12b285
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2024-01-13 18:13:27 +0600

    Create Set DS.cpp

---
+++ Set DS.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int row,col;
+    int a[4][2]={{3,2},{2,4},{5,6},{3,4}};
+    int i;
+    for(i=0;i<4;i++)
+    {
+        int j;
+        for(j=0;j<2;j++)
+        {
+        cout<<A[i][j];
+
+        }
+        cout<<A[i][j]<<endl;
+    }
+
+    return 0;
+}
