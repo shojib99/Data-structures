95db6fc79a894b5820a3548b1c7642bcb8df5515
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2024-02-07 13:40:13 +0600

    Create Array Display DSA.cpp

---
+++ Array display DSA.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int
+  num[4];
+  int i;
+  cout<<"Enter four number:"<<endl;
+ {
+  for( i=0;i<4;i++)
+   {
+     cin>>num[i];
+   }
+ }
+{
+  for(i=0;i<4;i++)
+   {
+     cout<<"Numbers are: "<<num[i]<<endl;
+   }
+}
+
+return 0;
+}
