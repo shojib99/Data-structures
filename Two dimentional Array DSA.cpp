231f861d778d255901136407aac6131169bf5cd0
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2024-02-07 13:28:02 +0600

    Create Two dimentional Array DSA.cpp

---
+++ Two dimentional Array DSA.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+ int arr[4][2]={{3,5},{4,7},{5,6},{5,8}};
+ int i,j;
+ cout<<"Two dimentional Array is:\n";
+ for(i=0; i<4; i++)
+  {
+   for(j=0; j<2; j++)
+    cout<<arr[i][j]<<" ";
+   cout<<endl;
+}
+cout<<endl;
+
+return 0;
+}
