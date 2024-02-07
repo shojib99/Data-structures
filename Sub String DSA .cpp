4749865032dd0c3242e365ecab38d903058838a6
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2024-02-07 20:01:35 +0600

    Create Sub String DSA .cpp

---
+++ Sub String DSA .cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ string str="Shojib";
+ string substring="";
+ substring=str.substr(0,7);
+ cout<<"sub string is:"<<substring;
+
+ return 0;
+}
