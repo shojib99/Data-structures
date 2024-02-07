aee60d7d23071784d4004415545450b31c995818
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2024-02-07 12:04:46 +0600

    Create Array print DSA.cpp

---
+++ Array print  DSA.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int arr[3];
+   int i;
+   cout<<"Enter 3 numbers: ";
+   for(i=0;i<3;i++)
+   {
+   cin>>arr[i];
+   }
+   for(i=0;i<3;i++)
+  {
+   cout<<"Number is: "<<arr[i]<<endl;
+  }
+
+}
