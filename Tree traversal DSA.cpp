7247b208ddae4d8260137eca2cc8a5d36dad85a9
Md. Shojib sheikh <128397983+shojib99@users.noreply.github.com>
2024-05-09 21:19:49 +0600

    Tree traversal DSA.cpp

---
+++ Tree traversal DSA.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+  int data;
+  struct Node *left,*right;
+  Node(int data)
+  {
+    this->data = data;
+    left=right= NULL;
+  }
+};
+
+void preorderTraversal(struct Node* node)
+ {
+  if (node == NULL)
+    return;
+
+  cout << node->data << "->";
+  preorderTraversal(node->left);
+  preorderTraversal(node->right);
+}
+
+void postorderTraversal(struct Node* node)
+{
+  if (node == NULL)
+    return;
+
+  postorderTraversal(node->left);
+  postorderTraversal(node->right);
+  cout << node->data << "->";
+}
+
+void inorderTraversal(struct Node* node)
+{
+  if (node == NULL)
+    return;
+
+  inorderTraversal(node->left);
+  cout << node->data << "->";
+  inorderTraversal(node->right);
+}
+
+int main()
+{
+  struct Node* root = new Node(5);
+  root->left = new Node(11);
+  root->right = new Node(8);
+  root->left->left = new Node(4);
+  root->left->right = new Node(7);
+
+  cout << "Inorder traversal ";
+  inorderTraversal(root);
+
+  cout << "\nPreorder traversal ";
+  preorderTraversal(root);
+
+  cout << "\nPostorder traversal ";
+  postorderTraversal(root);
+
+  return 0;
+}
